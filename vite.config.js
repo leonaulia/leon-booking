9e108f27c38ca84f08e98b047f6d13cac6472ea3
leonaulia <113237874+leonaulia@users.noreply.github.com>
2024-12-02 12:43:27 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      axios: require.resolve('axios'), // Explicitly resolve axios
+    },
+  },
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      external: ['axios'], // Externalize axios to avoid bundling issues
+    },
   },
 });
